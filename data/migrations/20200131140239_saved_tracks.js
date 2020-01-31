08194cc8b118f19f76e64622044143d229705584
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-01-31 14:54:11 -0800

    created database
    Co-Authored-By: April M. Clements <amc-ai@users.noreply.github.com>

--- data/migrations/20200131140239_saved_tracks.js
+++ data/migrations/20200131140239_saved_tracks.js
@@ -1,8 +1,30 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("saved_tracks", song => {
+    song.increments("saved_tracks_id");
+    song
+      .integer("userId")
+      .unsigned()
+      .notNullable()
+      .references("user_id")
+      .inTable("users_profile")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE");
+    song.varchar("items_added_at");
+    song.varchar("track_href");
+    song.varchar("track_id");
+    song.varchar("track_name");
+    song.integer("track_popularity");
+    song.varchar("track_preview_url");
+    song.integer("track_number");
+    song.varchar("track_uri");
+    song.varchar("albums_images");
+    song.varchar("artists_names");
+    song.varchar("artists_id");
+    song.integer("limit");
+    song.integer("total");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("saved_tracks");
 };
--- data/migrations/20200131140254_audio_features.js
+++ data/migrations/20200131140254_audio_features.js
@@ -1,8 +1,29 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("audio_features", audio => {
+    audio.increments("audio_id");
+    audio
+      .integer("savedTrackId")
+      .unsigned()
+      .notNullable()
+      .references("saved_tracks_id")
+      .inTable("saved_tracks")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE");
+    audio.float("acousticness");
+    audio.float("danceability");
+    audio.float("engery");
+    audio.integer("instrumentalness");
+    audio.integer("key");
+    audio.float("liveness");
+    audio.float("loudness");
+    audio.integer("mode");
+    audio.float("speechiness");
+    audio.float("tempo");
+    audio.integer("time_signature");
+    audio.float("valence");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("audio_features");
 };
